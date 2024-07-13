2fe621f14d3b0c83145e808935b46b45b822a8b0
Aryan Saxena <aryan11234567890@gmail.com>
2024-07-13 15:34:33 +0530

    First design of Login and Register page

--- Backend/DevDuelists/DevDuelists/settings.py
+++ Backend/DevDuelists/DevDuelists/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
